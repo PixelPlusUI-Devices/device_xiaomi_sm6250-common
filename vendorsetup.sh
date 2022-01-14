73c9c93aa42082b806494c9de91d9282633c576c
Abhishek001konni <abhishek001konni@gmail.com>
2022-01-14 20:19:25 +0530

    sm6250-common: Drop Hardware/xiaomi from Arrow

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
-rm -rf hardware/xiaomi
-git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.0 hardware/xiaomi
 git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.0 packages/apps/GCamGOPrebuilt
 git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
 git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
